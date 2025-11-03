a1340caf074094248e0ec0def87ee971bb24052d
shanweerasinghe3999-cmd <shanweerasinghe3999@gmail.com>
2025-11-03 16:31:54 +0530

    Create mergesort

---
+++ MergeSort.java
@@ -0,0 +1,37 @@
+public class MergeSort {
+    static int steps = 0;
+
+    public static void run(int[] arr) {
+        int[] data = arr.clone();
+        steps = 0;
+        long start = System.nanoTime();
+        mergeSort(data, 0, data.length - 1);
+        long end = System.nanoTime();
+        Utils.displayResults("Merge Sort", data, steps, end - start);
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            steps++;
+            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
+            else temp[k++] = arr[j++];
+        }
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        System.arraycopy(temp, 0, arr, left, temp.length);
+    }
+}
