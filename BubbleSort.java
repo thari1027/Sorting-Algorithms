b0e55fe9f659c9edfacbd3a1a1f5015383a06627
Tharindu Weerasinghe <tharindur817@gmail.com>
2025-11-03 14:47:59 +0530

    First commit

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+public class BubbleSort {
+    public static void run(int[] arr) {
+        int[] data = arr.clone();
+        long start = System.nanoTime();
+        int steps = 0;
+
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                steps++;
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+
+        long end = System.nanoTime();
+        Utils.displayResults("Bubble Sort", data, steps, end - start);
+    }
+}
